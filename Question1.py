d45ab874566d9afb8692d2c585d670386a16b3d2
Lucy Lin <lucylin6699@gmail.com>
2024-03-08 14:19:23 -0500

    Add various html files

--- Question1.py
+++ Question1.py
@@ -4,11 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Welcome, user, to my CSCB20 website!</h1>'
 
-@app.route('/user/<name>')
+@app.route('/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    name = name.strip()
+    name = name.swapcase()
+    return '<h1>Welcome, {}, to my CSCB20 website!</h1>'.format(name)
 
 if __name__ == '__main__':
     app.run(debug=True)

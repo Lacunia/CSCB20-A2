5c0e6c22fa819777bca0083bd94e4bb099c3c6b0
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-03-12 12:32:47 -0400

    modified python code

--- Question1.py
+++ Question1.py
@@ -10,7 +10,17 @@ def hello():
 def user(name):
     
     name = name.strip()
-    name = name.swapcase()
+    # name = name.swapcase()
+    if (name.isalpha()):
+        if (name.isupper()):
+            name = name.lower()
+        elif (name.islower()):
+            name = name.upper()
+        else:
+            name = name[0].upper() + name[1:].lower()
+    else:
+        name = name.strip("0123456789@#$%^&*()_+=-`~[]{}|;:,.<>?/\\")
+
     return '<h1>Welcome, {}, to my CSCB20 website!</h1>'.format(name)
 
 if __name__ == '__main__':

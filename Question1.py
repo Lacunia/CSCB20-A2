82c15c01e4b73b9e215b142edcb6a5f709bd7f1f
Lucy Lin <lucylin6699@gmail.com>
2024-03-12 13:46:51 -0400

    Updated Question1.py

--- Question1.py
+++ Question1.py
@@ -18,10 +18,15 @@ def user(name):
             name = name.upper()
         else:
             name = name[0].upper() + name[1:].lower()
-    else:
-        name = name.strip("0123456789@#$%^&*()_+=-`~[]{}|;:,.<>?/\\")
+        return '<h1>Welcome, {}, to my CSCB20 website!</h1>'.format(name)
+    else: 
+        new_name = ""
+        for char in name:
+            if (char not in "0123456789@#$%^&*()_+=-`~[]{}|;:,.<>?/\\"):
+                new_name = new_name + char
+        return '<h1>Welcome, {}, to my CSCB20 website!</h1>'.format(new_name)
 
-    return '<h1>Welcome, {}, to my CSCB20 website!</h1>'.format(name)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
